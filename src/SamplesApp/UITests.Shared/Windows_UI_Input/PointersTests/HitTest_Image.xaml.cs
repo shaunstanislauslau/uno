4b2a662464b541e240091836828b21ec14c45374
David <github.site@torick.net>
2020-07-02 18:39:39 -0400

    test(pointers): [WASM] Test the OriginalSource of pointer events for Image

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Input/PointersTests/HitTest_Image.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+#if __IOS__ || __MACOS__ || __ANDROID__
+using _FrameworkElement = Windows.UI.Xaml.IFrameworkElement;
+#else
+using _FrameworkElement = Windows.UI.Xaml.FrameworkElement;
+#endif
+
+namespace UITests.Windows_UI_Input.PointersTests
+{
+	[Sample("Pointers", "Image")]
+	public sealed partial class HitTest_Image : Page
+	{
+		public HitTest_Image()
+		{
+			this.InitializeComponent();
+
+			foreach (var elt in GetElements())
+			{
+				elt.PointerPressed += (snd, e) =>
+				{
+					e.Handled = true;
+					LastPressed.Text = elt.Name;
+					LastPressedSrc.Text = (e.OriginalSource as _FrameworkElement)?.Name ?? "-unknown-";
+				};
+				elt.PointerMoved += (snd, e) =>
+				{
+					e.Handled = true;
+					LastHovered.Text = elt.Name;
+					LastHoveredSrc.Text = (e.OriginalSource as _FrameworkElement)?.Name ?? "-unknown-";
+				};
+			}
+		}
+
+		private IEnumerable<_FrameworkElement> GetElements()
+		{
+			yield return Root;
+
+			yield return TheImage;
+		}
+	}
+}
